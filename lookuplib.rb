ddd186355699622324ece241d1a282e32c9f7665
teng.cong <congteng45@gmail.com>
2014-05-15 19:19:30 +0800

    bug fix: gemfile source, relative path

--- lookuplib.rb
+++ lookuplib.rb
@@ -2,6 +2,6 @@ require 'ffi'
 
 module LookupLib
   extend FFI::Library
-  ffi_lib "/Users/lifedim/Projects/scripts/rubydict/clib/liblookup.so"
+  ffi_lib "clib/liblookup.so"
   attach_function :lookup, [:string, :long, :long, :string], :string
 end
